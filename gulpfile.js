06a0e9d6b0d77888c08969b948ed51a6c8ba923a
lukas-sketrys <lukas.sketrys@debutify.com>
2022-03-17 18:12:09 +0200

    added structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
   const gulp = require('gulp');
   const sass = require('gulp-sass')(require('sass'));
   const themeKit = require('@shopify/themekit');
-  const clean = require('gulp-clean-css');
+  // const clean = require('gulp-clean-css');
 
   gulp.task('sass', function(){
     return gulp.src('styles/custom-styles.scss')
             .pipe(sass())
-            .pipe(clean({ compatibility: 'ie11'}))
+            // .pipe(clean({ compatibility: 'ie11'}))
             .pipe(gulp.dest('assets'))
   });
 
