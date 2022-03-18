0e554b3ff6c019cf86adccdeacc57f0cdda29de3
lukas-sketrys <lukas.sketrys@debutify.com>
2022-03-18 12:51:30 +0200

    updated files

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@
   // const clean = require('gulp-clean-css');
 
   gulp.task('sass', function(){
-    return gulp.src('styles/custom-styles.scss')
+    return gulp.src('sass/custom-styles.scss')
             .pipe(sass())
             // .pipe(clean({ compatibility: 'ie11'}))
             .pipe(gulp.dest('assets'))
   });
 
   gulp.task('watch', function(){
-    gulp.watch('styles/**/*.scss', gulp.series('sass'))
+    gulp.watch('sass/**/*.scss', gulp.series('sass'))
     themeKit.command('watch', {
         env: 'development'
     })
